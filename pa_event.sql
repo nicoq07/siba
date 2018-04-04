90d8ed2f2dadb79e97e1350a0ec3c59e41bcf308
Nicolás Quiroga <nicoq07@gmail.com>
2018-04-04 14:24:45 -0300

    SQL PA y evento
    para controlar la cantidad de alumnos por clases.

---
+++ pa_event.sql
@@ -0,0 +1,13 @@
+/* Encender el programador de eventos */
+SET GLOBAL event_scheduler="ON" ;
+
+/* Creo el procedimiento almacenado */
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizar_alumnos_por_clase`()
+BEGIN
+ update clases ,(SELECT c.id as id_clase , COUNT(ca.alumno_id) as cantidad_alumnos FROM clases_alumnos as ca, clases as c where c.id = ca.clase_id GROUP BY c.id) res set clases.alumno_count = res.cantidad_alumnos where clases.id = res.id_clase ;
+ END$$
+DELIMITER ;
+
+/* Creo el evento */
+CREATE DEFINER=`root`@`localhost` EVENT `evento_llamar_actualizar_alumnos_por_clase` ON SCHEDULE EVERY 6 HOUR STARTS '2018-04-04 00:00:00' ON COMPLETION PRESERVE ENABLE DO CALL actualizar_alumnos_por_clase()
