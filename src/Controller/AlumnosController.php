d4b5428f78ee9d203609079dca955b940f76ea75
nicoq07 <nicolasquiroga@MacBook-Pro-de-Nicolas.local>
2018-04-06 17:27:46 -0300

    Bug Insertar Seguimientos
    Desde el dia del cambio

--- src/Controller/AlumnosController.php
+++ src/Controller/AlumnosController.php
@@ -744,7 +744,7 @@ class AlumnosController extends AppController
 			if(!$claseAlumno->existeSeguimiento())
 			{
 				//Creo las fechas de incio y fin para  recorrerlas
-				$fechaInicio = strtotime($alumno->modified->format('Y-m-d'));
+				$fechaInicio = date('Y-m-d');
 				$fechaFin = strtotime($claseAlumno->clase->horario->ciclolectivo->fecha_fin->format('Y-m-d'));
 				
 				//recorro por dia hasta la fecha fin
