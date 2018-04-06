ce40b9147b8e7c8a6e48a37190f1e3735d7adbf6
nicoq07 <nicolasquiroga@MacBook-Pro-de-Nicolas.local>
2018-04-06 12:59:55 -0300

    Constantes del sistema

---
+++ config/constantes.php
@@ -0,0 +1,28 @@
+<?php 
+
+define('DS_WINDOWS_IMG', '/');
+//CONSTANTES DE ROLES
+define('ADMINISTRADOR', 1);
+define('PROFESOR', 2);
+define('OPERADOR', 3);
+
+//FIN CONSTANTES DE ROLES
+define('URGENTE', 1);
+define('NORMAL', 2);
+define('BAJA', 3);
+
+//Parametros
+define('ENVIAR_MAIL_AUTOMATICAMENTE', 'envio_mail_automatico');
+
+//Datos del lugar
+define('LOGO','logo.png');
+define('NOMBRE_SISTEMA','SIBA');
+define('NOMBRE_CORTO_EMPRESA','I.B.A Lugano');
+define('NOMBRE_LARGO_EMPRESA','Instituto Buenos Aires');
+define('ENCARGADO_EMPRESA','Dir. Profesor Hugo Castro');
+define('DIRECCION_EMPRESA','José León Suárez 5246 CP(1439) - Tel: 4638-5062');
+
+
+
+
+?>
