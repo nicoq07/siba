805072bc7886d9eac31a97ec8bf5b371e7138e6f
nicoq07 <nicolasquiroga@MacBook-Pro-de-Nicolas.local>
2018-04-18 18:40:40 -0300

    Constantes nuevas

--- config/constantes.php
+++ config/constantes.php
@@ -21,6 +21,10 @@ define('NOMBRE_CORTO_EMPRESA','I.B.A Lugano');
 define('NOMBRE_LARGO_EMPRESA','Instituto Buenos Aires');
 define('ENCARGADO_EMPRESA','Dir. Profesor Hugo Castro');
 define('DIRECCION_EMPRESA','José León Suárez 5246 CP(1439) - Tel: 4638-5062');
+define('FACEBOOK_EMPRESA',"www.facebook.com/ibalugano");
+define('CORREO_EMPRESA',"ibalugano@gmail.com");
+define('WEB_EMPRESA',"www.ibalugano.com.ar");
+define('CELULAR_EMPRESA',"1127135062");
 
 
 //Modulos
